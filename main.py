954327805dffbd247ce596d872412fc60a1e7ea5
Michael Hahn <skipiano@gmail.com>
2021-03-03 17:41:52 -0800

    fix bugs with lists, testing parse function

--- main.py
+++ main.py
@@ -4,27 +4,23 @@ import json
 folder_dict = {}
 
 # parses input file and outputs a json object of folder names corresponding to the elements inside
-# uses _ as the default delimiter
 
 
-def parse(file_name):
-    parse(file_name, '_')
-
-
-def parse(file_name, delimiter):
+def parse(file_name, delimiter='_'):
     with open(file_name) as csv_file:
         temp_names = csv.reader(csv_file)
-        names = [[]]
-        folders = [[]]
+        names = []
+        folders = []
         index = 0
         for name in temp_names:
-            names[index] = name.split(delimiter)
-            folders[index] = list(names[index])
+            names.append(name[0].split(delimiter))
+            folders.append(name[0].split(delimiter))
             # possible folder names should not have the last word to avoid having folders that contain empty strings
             folders[index].pop(len(folders[index])-1)
+            index += 1
         global folder_dict
         folder_names = group_folders(folders)
-        for i in range(names):
+        for i in range(len(names)):
             folder_name = delimiter.join(folder_names[i])
             elt_name = delimiter.join(names[i][len(folder_names[i]):])
             if folder_name in folder_dict:
@@ -71,3 +67,6 @@ def move_file(init_folder, dest_folder, file_name):
     else:
         folder_dict[init_folder].remove(file_name)
         folder_dict[dest_folder].append(file_name)
+
+
+print(parse("names.csv"))
